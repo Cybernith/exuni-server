d6744b51c374fc78a1dd7a108b215f133ac40270
soroosh morshedi <devolopersoroosh@gmail.com>
2024-08-30 15:03:51 +0330

    main app models compeleted

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from main.models import BUSINESS
+from main.models import Business, Store, Currency, Supplier
 
-admin.site.register(BUSINESS)
+admin.site.register(Business)
+admin.site.register(Store)
+admin.site.register(Currency)
+admin.site.register(Supplier)
--- main/models.py
+++ main/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.db.models import Q
-from django_jalali.db import models as jmodels
 
-from helpers.models import BaseModel
-from users.models import custom_upload_to
+from helpers.models import BaseModel, DECIMAL
+from users.models import custom_upload_to, User
 
 
-class BUSINESS(BaseModel):
+class Business(BaseModel):
     ONLINE_MARKET = 'om'
     COMMISSION_SELLER = 'cs'
     REPRESENTATION_MARKET = 'rm'
@@ -43,4 +41,81 @@ class BUSINESS(BaseModel):
             ('getOwn.business', 'مشاهده کسب و کار خود'),
             ('updateOwn.business', 'ویرایش کسب و کار خود'),
             ('deleteOwn.business', 'حذف کسب و کار خود'),
+        )
+
+
+class Store(BaseModel):
+
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='stores', blank=True, null=True)
+    is_central = models.BooleanField(default=False)
+
+    class Meta(BaseModel.Meta):
+        verbose_name = 'Store'
+        permission_basename = 'store'
+        permissions = (
+            ('get.store', 'مشاهده انبار'),
+            ('create.store', 'تعریف انبار'),
+            ('update.store', 'ویرایش انبار'),
+            ('delete.store', 'حذف انبار'),
+
+            ('getOwn.store', 'مشاهده انبار خود'),
+            ('updateOwn.store', 'ویرایش انبار خود'),
+            ('deleteOwn.store', 'حذف انبار خود'),
+        )
+
+
+class Currency(BaseModel):
+    name = models.CharField(max_length=150)
+    exchange_rate_to_rial = DECIMAL()
+
+    def exchange_to_rial(self, amount):
+        if amount:
+            return amount * self.exchange_rate_to_rial
+        else:
+            return 0
+
+    def exchange_rial_to_currency(self, amount):
+        if amount:
+            return amount / self.exchange_rate_to_rial
+        else:
+            return 0
+
+    class Meta(BaseModel.Meta):
+        verbose_name = 'Currency'
+        permission_basename = 'currency'
+        permissions = (
+            ('get.currency', 'مشاهده ارز'),
+            ('create.currency', 'تعریف ارز'),
+            ('update.currency', 'ویرایش ارز'),
+            ('delete.currency', 'حذف ارز'),
+
+            ('getOwn.currency', 'مشاهده ارز خود'),
+            ('updateOwn.currency', 'ویرایش ارز خود'),
+            ('deleteOwn.currency', 'حذف ارز خود'),
+        )
+
+
+class Supplier(BaseModel):
+    name = models.CharField(max_length=150)
+    bank_account_number = models.CharField(max_length=50, blank=True, null=True)
+    bank_card_number = models.CharField(max_length=50, blank=True, null=True)
+    bank_sheba_number = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=11, blank=True, null=True)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='supplies', blank=True, null=True)
+
+    class Meta(BaseModel.Meta):
+        verbose_name = 'Supplier'
+        permission_basename = 'supplier'
+        permissions = (
+            ('get.currency', 'مشاهده تامین کننده'),
+            ('create.currency', 'تعریف تامین کننده'),
+            ('update.currency', 'ویرایش تامین کننده'),
+            ('delete.currency', 'حذف تامین کننده'),
+
+            ('getOwn.currency', 'مشاهده تامین کننده خود'),
+            ('updateOwn.currency', 'ویرایش تامین کننده خود'),
+            ('deleteOwn.currency', 'حذف تامین کننده خود'),
         )
