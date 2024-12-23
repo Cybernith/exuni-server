1222da44e589c0f3e69c519a05fa1668c5377a44
soroosh morshedi <devolopersoroosh@gmail.com>
2024-12-23 12:02:10 +0330

    fix inventory and price change

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from products.models import Brand, Avail, ProductProperty, Category, Product, ProductGallery
+from products.models import Brand, Avail, ProductProperty, Category, Product, ProductGallery, ProductInventory
 
 admin.site.register(Brand)
 admin.site.register(Avail)
 admin.site.register(ProductProperty)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(ProductInventory)
 admin.site.register(ProductGallery)
