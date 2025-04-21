8298756b5ffaf5a5863e36496dcb0c7e2515f7bc
soroosh morshedi <devolopersoroosh@gmail.com>
2025-04-21 15:22:12 +0330

    add cms admin and naming

--- cms/urls.py
+++ cms/urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls import url
 
 from cms.views import HeaderElementApiView, AllHeaderElementApiView, PopUpElementApiView, AllPopUpElementApiView, \
-    BannerContentApiView, AllBannerContentApiView
+    BannerContentApiView, AllBannerContentApiView, HeaderElementDetailView, PopUpElementDetailView, \
+    BannerContentDetailView, BannerContentItemApiView, BannerContentItemDetailView
 
-app_name = 'products'
+app_name = 'cms'
 urlpatterns = [
     url(r'^currentHeaderElement$', HeaderElementApiView.as_view(), name='currentHeaderElement'),
     url(r'^headerElement$', AllHeaderElementApiView.as_view(), name='headerElement'),
+    url(r'^headerElement/(?P<pk>[0-9]+)$', HeaderElementDetailView.as_view(), name='headerElementDetail'),
 
     url(r'^currentPopUpElement$', PopUpElementApiView.as_view(), name='currentPopUpElement'),
     url(r'^popUpElement$', AllPopUpElementApiView.as_view(), name='popUpElement'),
+    url(r'^popUpElement/(?P<pk>[0-9]+)$', PopUpElementDetailView.as_view(), name='popUpElementDetail'),
 
     url(r'^currentBannerContent$', BannerContentApiView.as_view(), name='currentBannerContent'),
     url(r'^bannerContent$', AllBannerContentApiView.as_view(), name='bannerContent'),
+    url(r'^bannerContent/(?P<pk>[0-9]+)$', BannerContentDetailView.as_view(), name='bannerContentDetail'),
+
+    url(r'^bannerContentItem$', BannerContentItemApiView.as_view(), name='bannerContentItem'),
+    url(r'^bannerContentItem/(?P<pk>[0-9]+)$', BannerContentItemDetailView.as_view(), name='bannerContentItemDetail'),
 
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,14 +1,16 @@
 import datetime
 
 from django.db.models import Q
+from django.http import Http404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
 from rest_framework.response import Response
 from rest_framework import status
 
-from cms.models import HeaderElement, PopUpElement, BannerContent
-from cms.serializers import HeaderElementSerializer, PopUpElementSerializer, BannerContentSerializer
+from cms.models import HeaderElement, PopUpElement, BannerContent, BannerContentItem
+from cms.serializers import HeaderElementSerializer, PopUpElementSerializer, BannerContentSerializer, \
+    BannerContentItemSerializer
 from helpers.auth import BasicObjectPermission
 
 
@@ -39,6 +41,35 @@ class AllHeaderElementApiView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class HeaderElementDetailView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'header_element'
+
+    def get_object(self, pk):
+        try:
+            return HeaderElement.objects.get(pk=pk)
+        except HeaderElement.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        query = self.get_object(pk)
+        serializers = HeaderElementSerializer(query)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = self.get_object(pk)
+        serializer = HeaderElementSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        query = self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class PopUpElementApiView(APIView):
 
     def get(self, request):
@@ -50,7 +81,7 @@ class PopUpElementApiView(APIView):
 
 class AllPopUpElementApiView(APIView):
     permission_classes = (IsAuthenticated, BasicObjectPermission)
-    permission_basename = 'header_element'
+    permission_basename = 'pop_up_element'
 
     def get(self, request):
         query = PopUpElement.objects.all()
@@ -66,6 +97,35 @@ class AllPopUpElementApiView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class PopUpElementDetailView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'pop_up_element'
+
+    def get_object(self, pk):
+        try:
+            return PopUpElement.objects.get(pk=pk)
+        except PopUpElement.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        query = self.get_object(pk)
+        serializers = PopUpElementSerializer(query)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = self.get_object(pk)
+        serializer = PopUpElementSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        query = self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class BannerContentApiView(APIView):
 
     def get(self, request):
@@ -77,7 +137,7 @@ class BannerContentApiView(APIView):
 
 class AllBannerContentApiView(APIView):
     permission_classes = (IsAuthenticated, BasicObjectPermission)
-    permission_basename = 'header_element'
+    permission_basename = 'banner_content'
 
     def get(self, request):
         query = BannerContent.objects.all()
@@ -91,4 +151,76 @@ class AllBannerContentApiView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BannerContentDetailView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'banner_content'
+
+    def get_object(self, pk):
+        try:
+            return BannerContent.objects.get(pk=pk)
+        except BannerContent.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        query = self.get_object(pk)
+        serializers = BannerContentSerializer(query)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = self.get_object(pk)
+        serializer = BannerContentSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        query = self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BannerContentItemApiView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'banner_content_item'
+
+    def post(self, request):
+        data = request.data
+        serializer = BannerContentItemSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BannerContentItemDetailView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'banner_content_item'
+
+    def get_object(self, pk):
+        try:
+            return BannerContentItem.objects.get(pk=pk)
+        except BannerContentItem.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        query = self.get_object(pk)
+        serializers = BannerContentItemSerializer(query)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = self.get_object(pk)
+        serializer = BannerContentItemSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        query = self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
