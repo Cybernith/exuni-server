6761cefec0c746d4d10f7d770a2d5dabca8185c2
soroosh morshedi <devolopersoroosh@gmail.com>
2025-04-21 16:38:37 +0330

    fix bugs and add apis to shop

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import url
 
-from shop.views import CurrentUserCartApiView, CartDetailView
+from shop.views import CurrentUserCartApiView, CartDetailView, CurrentUserWishListApiView, WishListDetailView, \
+    CurrentUserComparisonApiView, ComparisonDetailView
 
 app_name = 'shop'
 urlpatterns = [
     url(r'^currentUserCart$', CurrentUserCartApiView.as_view(), name='currentUserCart'),
     url(r'^cart/(?P<pk>[0-9]+)$', CartDetailView.as_view(), name='cartDetail'),
+
+    url(r'^currentUserWishList$', CurrentUserWishListApiView.as_view(), name='currentUserWishList'),
+    url(r'^wishList/(?P<pk>[0-9]+)$', WishListDetailView.as_view(), name='wishListDetail'),
+
+    url(r'^currentUserComparison$', CurrentUserComparisonApiView.as_view(), name='currentUserComparison'),
+    url(r'^comparison/(?P<pk>[0-9]+)$', ComparisonDetailView.as_view(), name='comparisonDetail'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -8,8 +8,9 @@ from rest_framework import status
 
 from helpers.auth import BasicObjectPermission
 from helpers.functions import get_current_user
-from shop.models import Cart
-from shop.serializers import CartCRUDSerializer, CartRetrieveSerializer
+from shop.models import Cart, WishList, Comparison
+from shop.serializers import CartCRUDSerializer, CartRetrieveSerializer, WishListRetrieveSerializer, \
+    WishListCRUDSerializer, ComparisonRetrieveSerializer, ComparisonCRUDSerializer
 
 
 class CurrentUserCartApiView(APIView):
@@ -52,6 +53,108 @@ class CartDetailView(APIView):
         data = request.data
         data['customer'] = get_current_user().id
         serializer = CartCRUDSerializer(query, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        query = self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CurrentUserWishListApiView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'wish_list'
+
+    def get(self, request):
+        customer = get_current_user()
+        query = WishList.objects.filter(customer=customer)
+        serializers = WishListRetrieveSerializer(query, many=True, context={'request': request})
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data
+        data['customer'] = get_current_user().id
+        serializer = WishListCRUDSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class WishListDetailView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'wish_list'
+
+    def get_object(self, pk):
+        try:
+            return WishList.objects.get(pk=pk)
+        except WishList.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        query = self.get_object(pk)
+        serializers = WishListRetrieveSerializer(query)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = self.get_object(pk)
+        data = request.data
+        data['customer'] = get_current_user().id
+        serializer = WishListCRUDSerializer(query, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        query = self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CurrentUserComparisonApiView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'comparison'
+
+    def get(self, request):
+        customer = get_current_user()
+        query = Comparison.objects.filter(customer=customer)
+        serializers = ComparisonRetrieveSerializer(query, many=True, context={'request': request})
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data
+        data['customer'] = get_current_user().id
+        serializer = ComparisonCRUDSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ComparisonDetailView(APIView):
+    permission_classes = (IsAuthenticated, BasicObjectPermission)
+    permission_basename = 'comparison'
+
+    def get_object(self, pk):
+        try:
+            return Comparison.objects.get(pk=pk)
+        except Comparison.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        query = self.get_object(pk)
+        serializers = ComparisonRetrieveSerializer(query)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = self.get_object(pk)
+        data = request.data
+        data['customer'] = get_current_user().id
+        serializer = ComparisonCRUDSerializer(query, data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
