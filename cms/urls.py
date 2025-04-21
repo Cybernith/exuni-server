8490b9fb361d35b0500fb89afabd44f13415e7b3
soroosh morshedi <devolopersoroosh@gmail.com>
2025-04-21 14:59:15 +0330

    add cms views and urls

--- cms/urls.py
+++ cms/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
 
+from cms.views import HeaderElementApiView, AllHeaderElementApiView, PopUpElementApiView, AllPopUpElementApiView, \
+    BannerContentApiView, AllBannerContentApiView
 
 app_name = 'products'
 urlpatterns = [
-    #url(r'^brand$', BrandApiView.as_view(), name='brandApiView'),
+    url(r'^currentHeaderElement$', HeaderElementApiView.as_view(), name='currentHeaderElement'),
+    url(r'^headerElement$', AllHeaderElementApiView.as_view(), name='headerElement'),
+
+    url(r'^currentPopUpElement$', PopUpElementApiView.as_view(), name='currentPopUpElement'),
+    url(r'^popUpElement$', AllPopUpElementApiView.as_view(), name='popUpElement'),
+
+    url(r'^currentBannerContent$', BannerContentApiView.as_view(), name='currentBannerContent'),
+    url(r'^bannerContent$', AllBannerContentApiView.as_view(), name='bannerContent'),
 
 ]
