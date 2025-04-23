7b465a51d2d2668576b01e2d836e6ae6d7798ca8
soroosh morshedi <devolopersoroosh@gmail.com>
2025-04-23 17:50:59 +0330

    finished products api for shop

--- products/shop/filters.py
+++ products/shop/filters.py
@@ -12,6 +12,8 @@ class ShopProductFilter(filters.FilterSet):
     min_inventory = django_filters.NumberFilter(field_name='current_inventory__inventory', lookup_expr='gte')
     max_inventory = django_filters.NumberFilter(field_name='current_inventory__inventory', lookup_expr='lte')
     in_stock = django_filters.BooleanFilter(method='filter_in_stock')
+    properties = filters.CharFilter(method='filter_properties')
+    properties_in = filters.CharFilter(method='filter_properties_in')
 
     class Meta:
         model = Product
@@ -30,3 +32,22 @@ class ShopProductFilter(filters.FilterSet):
             return queryset.filter(
                 Q(current_inventory__inventory__lte=0) | Q(current_inventory__inventory__isnull=True)
             )
+
+        def filter_properties(self, queryset, name, value):
+            try:
+                ids = [int(i.strip()) for i in value.split(',') if i.strip().isdigit()]
+            except ValueError:
+                return queryset.none()
+
+            for property_id in ids:
+                queryset.filter(perperties__id=property_id)
+
+            return queryset
+
+        def filter_properties_in(self, queryset, name, value):
+            try:
+                ids = [int(i.strip()) for i in value.split(',') if i.strip().isdigit()]
+            except ValueError:
+                return queryset.none()
+
+            return queryset.filter(perperties__id__in=ids)
