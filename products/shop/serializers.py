8c78620ec146343c3333ad594cfcefbdecf614e4
soroosh morshedi <devolopersoroosh@gmail.com>
2025-04-23 19:55:39 +0330

    simmilar brand and related products algoritm finished

--- products/shop/serializers.py
+++ products/shop/serializers.py
@@ -82,8 +82,8 @@ class ShopProductDetailSerializers(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
     category = serializers.SerializerMethodField()
     brand = serializers.SerializerMethodField()
-    similar_products = serializers.SerializerMethodField()
-    similar_brand_products = serializers.SerializerMethodField()
+    #similar_products = serializers.SerializerMethodField()
+    #similar_brand_products = serializers.SerializerMethodField()
     offer_display = serializers.ReadOnlyField(source='offer_display')
     in_wish_list_count = serializers.ReadOnlyField(source='in_wish_list_count')
     #comments = serializers.SerializerMethodField()
@@ -120,8 +120,8 @@ class ShopProductDetailSerializers(serializers.ModelSerializer):
             'in_wish_list_count',
             #'comments',
             'comments_count',
-            'similar_products',
-            'similar_brand_products',
+            #'similar_products',
+            #'similar_brand_products',
         ]
 
     def get_image(self, obj):
@@ -140,19 +140,18 @@ class ShopProductDetailSerializers(serializers.ModelSerializer):
     def get_category(self, obj):
         return {'id': obj.category.id, 'name': obj.category.name} if obj.category else None
 
-    def get_similar_products(self, obj):
-        similar_products = Product.objects.filter(
-            Q(category=obj.category) |
-            Q(avails__in=obj.avails) |
-            Q(properties__in=obj.properties)
-        ).only('id', 'name', 'picture').exclude(id=obj.id)[:10]
+    #def get_similar_products(self, obj):
+    #    similar_products = Product.objects.filter(
+    #        Q(category=obj.category) |
+    #        Q(avails__in=obj.avails) |
+    #        Q(properties__in=obj.properties)
+    #    ).only('id', 'name', 'picture').exclude(id=obj.id)[:10]
+    #    return ShopSimilarProductsListSerializers(similar_products, many=True).data
 
-        return ShopSimilarProductsListSerializers(similar_products, many=True).data
-
-    def get_similar_brand_products(self, obj):
-        similar_products = Product.objects.filter(brand=obj.brand).only('id', 'name', 'picture').exclude(id=obj.id)[:10]
-
-        return ShopSimilarProductsListSerializers(similar_products, many=True).data
+    #def get_similar_brand_products(self, obj):
+    #    similar_products = Product.objects.filter(brand=obj.brand).only('id', 'name', 'picture').exclude(id=obj.id)[:10]
+    #
+    #    return ShopSimilarProductsListSerializers(similar_products, many=True).data
 
     #def get_comments(self, obj):
     #    comments = obj.product_comments.filter()[:4]
