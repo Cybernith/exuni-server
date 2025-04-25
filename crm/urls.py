5922c6a4a3d5c6f8488c474f1eddf9607361e3d6
soroosh morshedi <devolopersoroosh@gmail.com>
2025-04-25 15:57:49 +0330

    fix pep 8

--- crm/urls.py
+++ crm/urls.py
@@ -6,10 +6,9 @@ from crm.views import ShopProductViewLogApiView, ProductVisitReportView, Product
 
 app_name = 'crm'
 urlpatterns = [
-
+    url(r'^shopProductLog/(?P<product_id>[0-9]+)$', ShopProductViewLogApiView.as_view(), name='shopProductLog'),
     url(r'^shopProductViewsSummaryLogReport$', ShopProductViewLogReportListView.as_view(),
         name='shopProductViewsSummaryLogReport'),
-    url(r'^shopProductLog/(?P<product_id>[0-9]+)$', ShopProductViewLogApiView.as_view(), name='shopProductLog'),
     url(r'^shopProductViewsLogReport/(?P<product_id>[0-9]+)$', ProductVisitReportView.as_view(),
         name='shopProductViewsLogReport'),
     url(r'^shopProductViewsInRangeLogReport/(?P<product_id>[0-9]+)$', ProductInRangeVisitReportView.as_view(),
