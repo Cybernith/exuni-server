f365081153d98515b38b95e89c450c1cd625975d
soroosh morshedi <devolopersoroosh@gmail.com>
2025-04-25 17:34:44 +0330

    add view log

--- crm/functions.py
+++ crm/functions.py
@@ -1,4 +1,5 @@
 from crm.models import SearchLog, ShopProductViewLog
+from django.core.cache import cache
 
 
 def save_search_log(request, query_value):
@@ -13,15 +14,27 @@ def save_search_log(request, query_value):
     )
 
 
-def save_product_view_log(request, product, referer=''):
-    if not request.session.session_key:
-        request.session.save()
+def save_product_view_log(request, product):
+    if request.user.is_authenticated:
+        user_key = f"user:{request.user.id}"
+    else:
+        if not request.session.session_key:
+            request.session.save()
+        user_key = f"anon:{request.session.session_key}"
+
+    cache_key = f"viewed:{user_key}:product:{product.id}"
+
+    if cache.get(cache_key):
+        return
+
     ShopProductViewLog.objects.create(
         user_agent=request.META.get("HTTP_USER_AGENT", ""),
         user=request.user,
         product=product,
-        referer=referer,
         ip_address=request.META.get("REMOTE_ADDR", ""),
         session_key=request.session.session_key,
+        referer=request.META.get('HTTP_REFERER', "")
     )
+
+    cache.set(cache_key, True, timeout=3600)
 
