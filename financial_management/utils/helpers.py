3b4ff959ff841c57d9d79515996310ec4263943d
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-03 14:21:30 +0330

    financial services in progress until withdraw

--- financial_management/utils/helpers.py
+++ financial_management/utils/helpers.py
@@ -2,8 +2,9 @@ from decimal import Decimal
 
 from rest_framework.response import Response
 
-from financial_management.serivces.wallet_top_up import WalletTopUpService, WalletTopUpRequestService
-from financial_management.serivces.wallet_transfer_systhem import WalletTransferService
+from financial_management.serivces.wallet_top_up_service import WalletTopUpService, WalletTopUpRequestService
+from financial_management.serivces.wallet_transfer_service import WalletTransferService
+from financial_management.serivces.wallet_withdraw_service import WalletWithdrawRequestService, WalletWithdrawService
 
 
 def top_up_wallet_request(request, amount=None):
@@ -18,6 +19,26 @@ def top_up_wallet_request(request, amount=None):
 
 def top_up_wallet(request, amount=None):
     service = WalletTopUpService(
+        user=request.user,
+        amount=amount or Decimal(request.data["amount"]),
+        ip=request.META["REMOTE_ADDR"],
+        agent=request.META.get("HTTP_USER_AGENT")
+    )
+    return Response({"transaction_id": service.execute().id})
+
+
+def withdraw_wallet_request(request, amount=None):
+    service = WalletWithdrawRequestService(
+        user=request.user,
+        amount=amount or Decimal(request.data["amount"]),
+        ip=request.META["REMOTE_ADDR"],
+        agent=request.META.get("HTTP_USER_AGENT")
+    )
+    return Response({"message": service.execute()})
+
+
+def withdraw_wallet(request, amount=None):
+    service = WalletWithdrawService(
         user=request.user,
         amount=amount or Decimal(request.data["amount"]),
         ip=request.META["REMOTE_ADDR"],
