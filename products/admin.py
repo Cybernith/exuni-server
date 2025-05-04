c39f9bcdfdfd0b36aeecdcfbb8b718591ba4d526
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-04 13:34:02 +0330

    testing and fixing product in progress

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 
 from products.models import Brand, Avail, ProductProperty, Category, Product, ProductGallery, ProductInventory, \
-    ProductPrice, ProductPriceHistory
+    ProductPrice, ProductPriceHistory, ProductInventoryHistory
 
 admin.site.register(Brand)
 admin.site.register(Avail)
 admin.site.register(ProductProperty)
 admin.site.register(Category)
 admin.site.register(Product)
-admin.site.register(ProductInventory)
 admin.site.register(ProductGallery)
 
 
@@ -22,3 +21,15 @@ class ProductPriceHistoryAdmin(admin.ModelAdmin):
     list_display = ['product', 'previous_price', 'new_price', 'changed_at', 'changed_by']
     list_filter = ['product', 'changed_at', 'changed_by']
     search_fields = ['product__name']
+
+
+@admin.register(ProductInventory)
+class ProductInventoryAdmin(admin.ModelAdmin):
+    list_display = ['product', 'inventory', 'last_updated']
+
+
+@admin.register(ProductInventoryHistory)
+class ProductInventoryHistoryAdmin(admin.ModelAdmin):
+    list_display = ['inventory', 'previous_quantity', 'new_quantity', 'timestamp', 'changed_by']
+    list_filter = ['inventory', 'timestamp', 'changed_by']
+    search_fields = ['inventory']
