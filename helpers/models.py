5fd39ced03648d7859d46e32efaf3672a2d7fe98
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-12 16:00:51 +0330

    clear shit codes and make maigrate last changes

--- helpers/models.py
+++ helpers/models.py
@@ -19,16 +19,11 @@ options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('backward_financial_year', 'per
 
 class BaseManager(models.Manager):
 
-    def hasAccess(self, method, permission_basename=None, use_financial_year=True, financial_year=None, use_company=True, company=None):
+    def hasAccess(self, method, permission_basename=None):
         user = get_current_user()
 
         if not user:
             return super().get_queryset()
-
-        if hasattr(self.model, 'financial_year') and use_financial_year:
-            queryset = self.inFinancialYear(financial_year)
-        elif hasattr(self.model, 'company') and use_company:
-            queryset = self.inCompany(company)
         else:
             queryset = super().get_queryset()
 
