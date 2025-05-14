0205c704e21fc889b1978949ff5834ebded38ecc
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-15 01:04:22 +0330

    setup dokku cofs

--- server/configs.py
+++ server/configs.py
@@ -5,9 +5,9 @@ Databases = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'exuni_db',
         'USER': 'postgres',
-        'PASSWORD': 'slowj504',
-        'HOST': 'localhost',
-        'PORT': '3030',
+        'PASSWORD': '58936bf3904894e2a72ff75395401376',
+        'HOST': 'dokku-postgres-exuni-db',
+        'PORT': '5432',
         'ATOMIC_REQUESTS': True,
         'TEST': {
             'MIRROR': 'default',
@@ -17,8 +17,7 @@ Databases = {
 }
 
 client = MongoClient(
-    host='localhost',
-    port=27017
+    'mongodb://exuni-mongo-db:3b30316f2b64e75b2d4d02ca767bb13c@dokku-mongo-exuni-mongo-db:27017/exuni_mongo_db'
 )
 
 db_handle = client['sobhan_logs']
