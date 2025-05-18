510ae73303e4b20f222d185f99eaa5bb08106899
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-19 00:28:36 +0330

    fix sandbox for zarinpal

--- financial_management/zarinpal.py
+++ financial_management/zarinpal.py
@@ -1,13 +1,13 @@
 import requests
 
-from server.gateway_configs import ZARINPAL_MERCHANT_ID, GATEWAY_BASE_URL, GATEWAY_START_PAY_URL
+from server.gateway_configs import ZARINPAL_MERCHANT_ID, GATEWAY_BASE_URL, GATEWAY_START_PAY_URL, \
+    GATEWAY_DEVELOPMENT_URL, GATEWAY_DEVELOPMENT_START_PAY_URL
 
 
 class ZarinpalGateway:
-    GATEWAY_BASE_URL = GATEWAY_BASE_URL
-    GATEWAY_BASE_URL = GATEWAY_BASE_URL
+    GATEWAY_BASE_URL = GATEWAY_DEVELOPMENT_URL
     MERCHANT_ID = ZARINPAL_MERCHANT_ID
-    GATEWAY_START_PAY_URL = GATEWAY_START_PAY_URL
+    GATEWAY_START_PAY_URL = GATEWAY_DEVELOPMENT_START_PAY_URL
 
     def __init__(self, amount, description, callback_url, payment_id=None, email=None, mobile=None):
         self.amount = amount
