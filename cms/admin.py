f0532637b0d9f9ec99f7d44ec931e623a2f64671
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-18 15:29:19 +0330

    fix

--- cms/admin.py
+++ cms/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from cms.models import HeaderElement, PopUpElement, BannerContent, BannerContentItem
+from cms.models import HeaderElement, PopUpElement, BannerContent, BannerContentItem, ShopHomePageStory
 
 admin.site.register(HeaderElement)
 admin.site.register(PopUpElement)
 admin.site.register(BannerContent)
 admin.site.register(BannerContentItem)
+admin.site.register(ShopHomePageStory)
