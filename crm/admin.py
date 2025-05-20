6854205ffd3f3c62d46bcb38fcf321fbfd679862
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-20 17:31:18 +0330

    add notification to admin

--- crm/admin.py
+++ crm/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from crm.models import ShopProductViewLog
+from crm.models import ShopProductViewLog, SearchLog, Notification, UserNotification
 
 admin.site.register(ShopProductViewLog)
+admin.site.register(SearchLog)
+admin.site.register(Notification)
+admin.site.register(UserNotification)
