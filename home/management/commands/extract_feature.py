330fdcac1ab9c94f594e983507c87d6e558f5f27
soroosh morshedi <devolopersoroosh@gmail.com>
2025-05-30 01:03:24 +0330

    fix

---
+++ home/management/commands/extract_feature.py
@@ -0,0 +1,25 @@
+from django.core.management import BaseCommand
+
+from products.models import Product
+from products.utils import ImageFeatureExtractor
+import numpy as np
+
+extractor = ImageFeatureExtractor()
+
+class Command(BaseCommand):
+    help = 'extract feature'
+
+    def handle(self, *args, **options):
+        for product in Product.objects.all():
+            if product.picture and not product.feature_vector:
+                try:
+                    features = extractor.extract_features(product.picture)
+                    product.feature_vector = features.astype(np.float32).tobytes()
+                    product.save(update_fields=['feature_vector'])
+                except Exception as exception:
+                    print(exception)
+
+
+
+
+
