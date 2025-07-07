b4206028bcb80af4f977c4e22c1d45fed278ad95
soroosh morshedi <devolopersoroosh@gmail.com>
2025-07-07 09:53:18 +0330

    change pattern

--- crm/faraz_sms.py
+++ crm/faraz_sms.py
@@ -25,7 +25,7 @@ class IPPanelSMSService:
         otp_code = self.generate_code()
         url = f"{IPPANEL_BASE_URL}/sms/pattern/normal/send"
         payload = {
-            "code": 'pensvrur5oduf3l',
+            "code": '5f4j583dk863mtb',
             "sender": FARAZ_SMS_LINE_NUMBER,
             "recipient": phone,
             "variable": {
