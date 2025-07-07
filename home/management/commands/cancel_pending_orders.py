35fdc62509342ab5d20e72460cf2d28dac6d47df
soroosh morshedi <devolopersoroosh@gmail.com>
2025-07-07 12:04:50 +0330

    fix

--- home/management/commands/cancel_pending_orders.py
+++ home/management/commands/cancel_pending_orders.py
@@ -1,12 +1,14 @@
 from django.core.management import BaseCommand
+from django.db.models import Q
 
 from shop.models import ShopOrder
-
+from datetime import datetime, timedelta
 
 class Command(BaseCommand):
     help = 'cancel pending orders'
 
     def handle(self, *args, **options):
-        shop_orders = ShopOrder.objects.filter(status=ShopOrder.PENDING)
+        datetime_check = datetime.now() - timedelta(hours=5)
+        shop_orders = ShopOrder.objects.filter(Q(status=ShopOrder.PENDING) & Q(date_time__lt=datetime_check))
         for order in shop_orders:
             order.cancel_order()
