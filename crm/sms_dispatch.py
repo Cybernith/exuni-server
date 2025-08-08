8f5a1623fd5d4f3c34e187abfd4a32a2a1599e65
soroosh morshedi <devolopersoroosh@gmail.com>
2025-08-08 14:29:43 +0330

    fix

--- crm/sms_dispatch.py
+++ crm/sms_dispatch.py
@@ -19,14 +19,13 @@ class SMSHandler:
         shop_order = ShopOrder.objects.get(id=order)
         detail = shop_order.shipment_address
         sms_lines = [
-            "اکسونی EXUNI ",
+            "اکسونی ",
             "{} {}  عزیز".format(detail.first_name or '', detail.last_name or ''),
             "سفارش شما با موفقیت در اکسونی ثبت شد!",
             "شماره پیگیری سفارش شما: {}".format(shop_order.exuni_tracking_code),
             "مبلغ سفارش: {} تومان".format(add_separator(shop_order.final_amount)),
             "سفارش در حال آماده‌سازی برای بسته‌بندی هوشمند است.",
             "با هم می‌مانیم، در کنار هم می‌درخشیم.",
-            "exuni.ir"
         ]
 
         sms_text = "\n".join(sms_lines)
@@ -41,13 +40,12 @@ class SMSHandler:
         for order in shop_orders:
             detail = order.shipment_address
             sms_lines = [
-                "اکسونی EXUNI ",
+                "اکسونی ",
                 "{} {}  عزیز".format(detail.first_name or '', detail.last_name or ''),
                 "اکسونی در حال بسته‌بندی و آماده‌سازی سفارش شماست!",
                 "سفارش شما توسط {} با کد پیگیری {}".format(order.packager.name, order.exuni_tracking_code),
                 "در صف بسته‌بندی قرار گرفت و به زودی به پست ارسال می‌شود",
                 "صبر شما تضمین کیفیت ماست",
-                "exuni.ir/profile/orders",
             ]
             sms_text = "\n".join(sms_lines)
             messages.append(sms_text)
@@ -64,7 +62,7 @@ class SMSHandler:
         for order in shop_orders:
             detail = order.shipment_address
             sms_lines = [
-                "اکسونی EXUNI ",
+                "اکسونی ",
                 "{} {}  عزیز".format(detail.first_name or '', detail.last_name or ''),
                 "سفارش شما در اکسونی به پست ارسال شد!",
                 " کد رهگیری پست {}".format(order.post_tracking_code),
