596dff48b81379b446fa6a87585a1683f8839803
soroosh morshedi <devolopersoroosh@gmail.com>
2025-08-08 16:31:39 +0330

    fix

--- home/management/commands/showcount.py
+++ home/management/commands/showcount.py
@@ -1,26 +1,21 @@
 import jdatetime
 from django.core.management import BaseCommand
-from shop.models import ShopOrder
+
+from financial_management.models import Payment
 
 
 class Command(BaseCommand):
     help = 'expired pending orders'
 
     def handle(self, *args, **options):
-        year, month, day = map(int, '1404-05-06'.split('-'))
-        jdate = jdatetime.date(year, month, day)
-        start = jdate.togregorian()
-
-        year, month, day = map(int, '1404-05-12'.split('-'))
+        year, month, day = map(int, '1404-05-17'.split('-'))
         jdate = jdatetime.date(year, month, day)
         end = jdate.togregorian()
-        orders = ShopOrder.objects.filter(status__in=['pa', 'pr'], date_time__date__lte=end, date_time__date__gte=start)
-        customer_ids = orders.values_list('customer_id', flat=True).distinct()
 
-        print(
-            ShopOrder.objects.filter(
-                status__in=['pa', 'pr'], date_time__date__lte=end, date_time__date__gte=start
-            ).count()
-        )
+        year, month, day = map(int, '1404-05-1'.split('-'))
+        jdate = jdatetime.date(year, month, day)
+        start = jdate.togregorian()
+        payments = Payment.objects.filter(status__in=['pa', 'pr'], paid_at__date__lte=end, paid_at__date__gte=start)
+        customer_ids = payments.values_list('user_id', flat=True).distinct()
 
         print(len(customer_ids))
