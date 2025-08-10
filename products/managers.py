16866544ad4b41d279b0ce0a44bdc11f7b3ae834
soroosh morshedi <devolopersoroosh@gmail.com>
2025-08-10 22:28:02 +0330

    fix

--- products/managers.py
+++ products/managers.py
@@ -82,6 +82,7 @@ class ProductQuerySet(models.QuerySet):
         return (
             self.filter(
                 status=Product.PUBLISHED,
+                price__gt=0,
                 product_type__in=[Product.SIMPLE, Product.VARIABLE]
             )
             .annotate(
