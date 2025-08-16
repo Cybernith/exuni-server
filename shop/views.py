22f45ed6334bc17db3049c1fa44ec1f26e15eea4
soroosh morshedi <devolopersoroosh@gmail.com>
2025-08-16 10:47:36 +0330

    fix done

--- shop/views.py
+++ shop/views.py
@@ -482,8 +482,6 @@ class ShopOrderRegistrationView(APIView):
     throttle_classes = [ShopOrderRateThrottle]
 
     def post(self, request):
-        return Response({'message': 'دقایقی صبر کنید'}, status=status.HTTP_400_BAD_REQUEST)
-
         customer = get_current_user()
         data = request.data
         address_id = data.get('address')
