dcb55e797aa1c63c7bea94f512d4b70a058684f7
soroosh morshedi <devolopersoroosh@gmail.com>
2025-08-19 10:05:35 +0330

    test

--- home/management/commands/set_packing.py
+++ home/management/commands/set_packing.py
@@ -1,10 +1,7 @@
 from django.core.management import BaseCommand
 from django.db import transaction as db_transaction
 
-from server.store_configs import PACKING_STORE_ID
-from store_handle.models import ProductStoreInventory
-from products.models import ProductInventory
-from users.models import User
+from store_handle.models import  ProductStoreInventoryHandle
 
 
 class Command(BaseCommand):
@@ -12,11 +9,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with db_transaction.atomic():
-            user = User.objects.get(username='soroosh')
-            inventorties = ProductInventory.objects.select_for_update().all()
-            for inv in inventorties:
-                packing_inv, created = ProductStoreInventory.objects.select_for_update().get_or_create(
-                    product=inv.product, store_id=PACKING_STORE_ID
-                )
-                packing_inv.handle_inventory_in_store(val=inv.inventory, user=user)
-                print(packing_inv.product.name, ' done ', flush=True)
+            handles = ProductStoreInventoryHandle.objects.exclude(store_id=1)
+            for handle in handles:
+                handle.apply()
+                print('applied')
+
