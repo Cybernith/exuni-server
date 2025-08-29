609376ca99a9e9c4d9ce871fec22a1672ea0a579
soroosh morshedi <devolopersoroosh@gmail.com>
2025-08-29 21:32:48 +0330

    fix

--- home/management/commands/testsms.py
+++ home/management/commands/testsms.py
@@ -1,37 +1,35 @@
 import jdatetime
 
-from financial_management.models import Wallet
 from helpers.sms import Sms
 from django.core.management import BaseCommand
+
+from users.models import User
 
 
 class Command(BaseCommand):
     help = 'expired pending orders'
 
     def handle(self, *args, **options):
-        from shop.models import ShopOrder
-        year, month, day = map(int, '1404-05-06'.split('-'))
-        jdate = jdatetime.date(year, month, day)
-        start = jdate.togregorian()
+        from datetime import datetime
 
-        year, month, day = map(int, '1404-05-12'.split('-'))
-        jdate = jdatetime.date(year, month, day)
-        end = jdate.togregorian()
-        shop_orders = ShopOrder.objects.filter(status__in=['pa', 'pr'], date_time__date__lte=end, date_time__date__gte=start).select_related('shipment_address')
+        start = datetime(2025, 9, 27, 19, 0)
+        end = datetime(2025, 9, 28, 21, 0)
 
-        for order in shop_orders:
-            detail = order.shipment_address
-            sms_lines = [
-                "اکسونی ",
-                "{} {}  عزیز".format(detail.first_name or '', detail.last_name or ''),
-                "با ۱۰ روز صبر شما پردازش و بسته بندی اکسونی هوشمند شد!",
-                "این صبر دلیلی برای سرعت، دقت و پردازش بهینه سفارشات شماست",
-                "از امروز ارسال سفارشات اکسونی روزانه شد",
-                "به پاس شکیبایی شما",
-                "مبلغ : 500,000 + ریال",
-                "به کیف پول شما واریز شد",
-                "از امروز سفارشات در حال ارسال است",
-            ]
-            sms_text = "\n".join(sms_lines)
-            Sms.send(phone=order.customer.username, message=sms_text)
+        users = User.objects.filter(
+            verificationCodes__created_at__gte=start,
+            verificationCodes__created_at__lte=end
+        ).distinct()
+        sms_lines = [
+            "اکسونی ",
+            "همراه همیشگی اکسونی",
+            " مشکل ارسال پیامک ورود رفع شد… یک عالمه سورپرایز منتظر خرید توعه",
+        ]
+        sms_text = "\n".join(sms_lines)
+        Sms.send(phone='09177557674', message=sms_text)
+        Sms.send(phone='09035894088', message=sms_text)
+        Sms.send(phone='09309066484', message=sms_text)
+
+        for user in users:
+            Sms.send(phone=user.username, message=sms_text)
+
 
