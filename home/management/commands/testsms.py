2183462c2c1b8c4c52cea6eedac8337526f68f72
soroosh morshedi <devolopersoroosh@gmail.com>
2025-08-29 21:47:28 +0330

    fix

--- home/management/commands/testsms.py
+++ home/management/commands/testsms.py
@@ -12,23 +12,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         from datetime import datetime
 
-        start = datetime(2025, 9, 27, 19, 0)
-        end = datetime(2025, 9, 28, 21, 0)
+        start = datetime(2025, 8, 28, 20, 0)
+        end = datetime(2025, 8, 29, 22, 0)
 
         users = User.objects.filter(
             verificationCodes__created_at__gte=start,
             verificationCodes__created_at__lte=end
         ).distinct()
+        print(users.count())
         sms_lines = [
             "اکسونی ",
             "همراه همیشگی اکسونی",
             " مشکل ارسال پیامک ورود رفع شد… یک عالمه سورپرایز منتظر خرید توعه",
         ]
         sms_text = "\n".join(sms_lines)
-        Sms.send(phone='09177557674', message=sms_text)
-        Sms.send(phone='09035894088', message=sms_text)
-        Sms.send(phone='09309066484', message=sms_text)
-
         for user in users:
             Sms.send(phone=user.username, message=sms_text)
 
